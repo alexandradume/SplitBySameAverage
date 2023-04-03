d40bb5ee0660c0ea6713a6cbaf9213a6d56d31b0
alexandradume <alexandra.teodora.dume@stud.ubbcluj.ro>
2023-04-03 20:07:30 +0300

    Split An Array in two with same average

--- main.py
+++ main.py
@@ -4,32 +4,34 @@ class Solution(object):
     def splitArraySameAverage(self, A):
         # Assume that B is the smaller array
         lengthOfA= len(A)
+        # we initialize the length of B with the maximum value
         lengthOfB= int(lengthOfA / 2)
-        totalSum = sum(A)
+        sumOfA = sum(A)
         # We know that , sumOfA/lengthOfA = sumOfB / lengthOfB = sumOfC / lengthOfC, so
         # sumOfA*lengthOfB/lengthOfA = sumOfB,
         # knowing that sumOfB is an integer, it follows that sumOfA*lengthOfB%lengthOfA equals to 0
         # early pruning
         verify = False
         for index in range(1, lengthOfB + 1):
-            if totalSum * index % lengthOfA == 0:
+            if sumOfA * index % lengthOfA == 0:
                 verify = True
                 break
         # if no index can be found that satisfies the condition, the method returns false
         if not verify:
             return False
-        # After that, all the possible combinations of the sum of index numbers from the table are generated
+        # After that, all possible combination sum of lengthOfB numbers from the array using dynamic
+        # programming are generated
         sums = [set() for _ in range(lengthOfB + 1)]
         sums[0].add(0)
         for num in A:
             for index in range(lengthOfB, 0, -1):
                 for t in sums[index - 1]:
                     sums[index].add(t + num)
-        # if an index is found that satisfies the condition totalSum * index % lengthOfA == 0 and we have
-        # on the index position in sums hashset the value totalSum * index / lengthOfA the method returns
+        # if an index is found that satisfies the condition sumOfA * index % lengthOfA == 0 and we have
+        # on the index position in sums hashset the value sumOfA * index / lengthOfA the method returns
         # true
         for index in range(1, lengthOfB + 1):
-            if totalSum * index % lengthOfA == 0 and int(totalSum * index / lengthOfA) in sums[index]:
+            if sumOfA * index % lengthOfA == 0 and int(sumOfA * index / lengthOfA) in sums[index]:
                 return True
         # method could not find a solution
         return False
