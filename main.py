f68b082933082357430248f95b423c5b86639782
alexandradume <alexandra.teodora.dume@stud.ubbcluj.ro>
2023-04-03 19:31:31 +0300

    Split An Array in two with same average

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ class Solution(object):
         lengthOfA= len(A)
         lengthOfB= int(lengthOfA / 2)
         totalSum = sum(A)
-        # We know that , sumOfA/lengthOfB = sumOfB / lengthOfB = sumOfC / lengthOfC, so
-        # sumOfA*lengthOfB/lengthOfB = sumOfB,
-        # knowing that sumOfB is an integer, it follows that sumOfA*lengthOfB%lengthOfB equals to 0
+        # We know that , sumOfA/lengthOfA = sumOfB / lengthOfB = sumOfC / lengthOfC, so
+        # sumOfA*lengthOfB/lengthOfA = sumOfB,
+        # knowing that sumOfB is an integer, it follows that sumOfA*lengthOfB%lengthOfA equals to 0
+        # early pruning
         verify = False
         for index in range(1, lengthOfB + 1):
             if totalSum * index % lengthOfA == 0:
@@ -17,7 +18,7 @@ class Solution(object):
         # if no index can be found that satisfies the condition, the method returns false
         if not verify:
             return False
-        # After that, all the possible combinations of the sum of k numbers from the table are generated
+        # After that, all the possible combinations of the sum of index numbers from the table are generated
         sums = [set() for _ in range(lengthOfB + 1)]
         sums[0].add(0)
         for num in A:
@@ -25,7 +26,8 @@ class Solution(object):
                 for t in sums[index - 1]:
                     sums[index].add(t + num)
         # if an index is found that satisfies the condition totalSum * index % lengthOfA == 0 and we have
-        # on the index position in sums hashset the value totalSum * index / lengthOfA the method returns true
+        # on the index position in sums hashset the value totalSum * index / lengthOfA the method returns
+        # true
         for index in range(1, lengthOfB + 1):
             if totalSum * index % lengthOfA == 0 and int(totalSum * index / lengthOfA) in sums[index]:
                 return True
@@ -33,9 +35,9 @@ class Solution(object):
         return False
 
 
-def teste():
+def tests():
     # this method is one that tests some arrays that have or don't have values that can
-    #        # be split into two arrays with the same average
+    # be split into two arrays with the same average
     print("Testing started")
     solution = Solution()
     assert (solution.splitArraySameAverage([1, 1]) == True)
@@ -49,6 +51,6 @@ def teste():
     print("Passed..")
 
 if __name__ == '__main__':
-    teste()
+    tests()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
